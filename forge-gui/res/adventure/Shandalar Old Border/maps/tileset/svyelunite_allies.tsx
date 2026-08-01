f5c3ae453d76902d5412666ecc9cbb360cde2616
Vanja <vanja.ivancevic@gmail.com>
2026-08-01 08:42:01 +0100

    Old Border Shandalar: Improve Svyelunite sprite clarity

---
+++ forge-gui/res/adventure/Shandalar Old Border/maps/tileset/svyelunite_allies.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="svyelunite_allies" tilewidth="128" tileheight="96" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="112" height="96" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/deep_spawn_idle.png"/>
+ </tile>
+ <tile id="1">
+  <image width="80" height="48" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/floodwater_homarid_placeholder_idle.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="32" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/homarid_green_idle.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="32" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/homarid_purple_idle.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="32" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/homarid_red_idle.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/homarid_shaman_idle.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="64" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/homarid_spawning_bed_placeholder_idle.png"/>
+ </tile>
+ <tile id="7">
+  <image width="80" height="80" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/homarid_warrior_idle.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="32" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/homarid_yellow_idle.png"/>
+ </tile>
+ <tile id="9">
+  <image width="80" height="64" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/river_merfolk_placeholder_idle.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="48" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/seasinger_idle.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/svyelunite_priest_idle.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/viscerid_drone_placeholder_idle.png"/>
+ </tile>
+ <tile id="13">
+  <image width="80" height="80" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/vodalian_knights_idle.png"/>
+ </tile>
+ <tile id="14">
+  <image width="48" height="64" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/vodalian_mage_placeholder_idle.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/vodalian_merchant_placeholder_idle.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/vodalian_soldier_idle.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="96" source="../../sprites/enemy/fallen_empires/svyelunite/friendly_static/vodalian_war_machine_placeholder_idle.png"/>
+ </tile>
+</tileset>
